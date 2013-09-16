d85610171b3e1cce23196ab07c634ceacad7fd1e
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:24:02 -0500

    Solved First Name problem

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -4,5 +4,20 @@ Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
 
+        Dim fullName As String
+        ' Read the input from the user
+        fullName = txtFullName.Text
+
+        'Find the index of first space
+        Dim idxSpace As Integer
+        idxSpace = fullName.IndexOf(" ")
+
+        'extract the first name
+        Dim FirstName As String
+        FirstName = fullName.Substring(0, idxSpace)
+
+        'Display the result to the user
+        txtFirstName.Text = FirstName
+
     End Sub
 End Class
